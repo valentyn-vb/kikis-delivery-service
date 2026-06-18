aa0295c2049eeec7fb2680aa0e452e2605b2012d
Valentyn Beznosko <valikbeznosko@gmail.com>
2026-06-18 13:31:34 +0200

    install shadcn lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import { Cherry_Bomb_One } from "next/font/google";
+import { Cherry_Bomb_One, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const cherryBomb = Cherry_Bomb_One({
   weight: "400",
@@ -12,10 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cherryBomb.className}>
+    <html lang="en" className={cn(cherryBomb.className, "font-sans", geist.variable)}>
       <body>
         <header>
-          <h1>Kiki’s Delivery Service</h1>
+          <h1 className="text-3xl font-bold">Kiki’s Delivery Service</h1>
         </header>
         {children}
       </body>
